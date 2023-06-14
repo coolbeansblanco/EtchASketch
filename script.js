83d11d90bf5335d3eb083b9299ed82965ac62c6c
Alex Blanco <53310027+coolbeansblanco@users.noreply.github.com>
2023-06-13 18:41:26 -0600

    implement 16x16 div container through javascript

---
+++ script.js
@@ -0,0 +1,17 @@
+const divContainer = document.createElement('div');
+const body = document.querySelector('body');
+
+body.appendChild(divContainer);
+
+const arraySize = 16;
+
+//this loop creates 16 new rows in the div container
+for (i=0; i<arraySize; i++){
+    const divRow = document.createElement('div');
+    //this loop fills each row with 16 divs
+    for (j=0; j<arraySize; j++){
+        const div = document.createElement('div');
+        divRow.appendChild(div);
+    }
+    divContainer.appendChild(divRow);
+}
