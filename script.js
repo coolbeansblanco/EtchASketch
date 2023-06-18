543d4354e820d1f128f9db71d0a68e919ada8afe
Alex Blanco <53310027+coolbeansblanco@users.noreply.github.com>
2023-06-18 03:13:04 -0600

    refactor script to have user-determined matrix size

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
 
-
-const divContainer = document.createElement('div');
 const body = document.querySelector('body');
+const button = document.createElement('button');
 
-body.appendChild(divContainer);
+body.appendChild(button);
 
-divContainer.classList.add('container');
-const arraySize = 16;
+button.addEventListener('click', getNewArraySize);
 
-//this loop creates 16 new rows in the div container
-for (i=0; i<arraySize; i++){
-    const divRow = document.createElement('div');
-    //this loop fills each row with 16 divs
-    for (j=0; j<arraySize; j++){
-        const div = document.createElement('div');
-        div.addEventListener('mouseenter', ()=>{
-            div.classList.add('hover');
-        })
-        div.addEventListener('mouseleave', ()=>{
-            div.classList.remove('hover');
-        })
-        divRow.appendChild(div);
+
+makeArray(16);
+
+function makeArray(size){
+    const divContainer = document.createElement('div');
+    divContainer.classList.add('container');
+    //this loop creates 'size' amount of new rows in the div container
+    
+    for (i=0; i<size; i++){
+        const divRow = document.createElement('div');
+        //this loop fills each row with 'size' amount of divs
+        for (j=0; j<size; j++){
+            const div = document.createElement('div');
+            div.addEventListener('mouseenter', ()=>{
+                div.classList.add('hover');
+            })
+            div.addEventListener('mouseleave', ()=>{
+                div.classList.remove('hover');
+            })
+            divRow.appendChild(div);
+        }
+        divContainer.appendChild(divRow);
     }
-    divContainer.appendChild(divRow);
+    body.appendChild(divContainer);
+}
+
+function getNewArraySize(){
+    input = prompt("Enter size of Etch-A-Sketch (must be between 1-100): ");
+    if (input !== NaN && input > 0 && input < 101){
+        body.removeChild(body.lastChild);
+        makeArray(input);
+    }
+    else alert("Invalid input.");
 }
